a1dd8e9b75e5fb4713426f4ad56e6c1da36b9e6d
Artem Demo <artemdemo@gmail.com>
2018-08-02 07:17:01 +0300

    show and hide anchors

--- source/canvas/Arrow/Arrow.js
+++ source/canvas/Arrow/Arrow.js
@@ -1,6 +1,8 @@
 import Konva from 'konva';
 import Anchor from './Anchor';
 
+const STROKE_WIDTH = 8;
+
 class Arrow {
     constructor(mainStage) {
         this._stage = mainStage;
@@ -8,6 +10,9 @@ class Arrow {
         this._anchorLayer = null;
         this._anchors = null;
         this._quadPath = null;
+        // this._arrowHead = null;
+
+        this._hideTimeout = null;
     }
 
     drawArrow = () => {
@@ -24,7 +29,7 @@ class Arrow {
         if (!this._quadPath) {
             this._quadPath = new Konva.Path({
                 stroke: 'red',
-                strokeWidth: 8,
+                strokeWidth: STROKE_WIDTH,
                 data: pathStr,
             });
             this._quadPath.on('mouseover', () => {
@@ -33,14 +38,35 @@ class Arrow {
                 this._anchors.end.visible(true);
                 this._anchorLayer.draw();
             });
+            this._quadPath.on('mouseout', this.anchorOut);
             this._curveLayer.add(this._quadPath);
+
+            // this._arrowHead = new Konva.Line({
+            //     points: [startAnchorPos.x, startAnchorPos.y, 20, 20],
+            //     stroke: 'blue',
+            //     strokeWidth: STROKE_WIDTH,
+            //     lineCap: 'round',
+            //     lineJoin: 'round',
+            // });
+            // this._curveLayer.add(this._arrowHead);
         }
         this._quadPath.setData(pathStr);
         this._quadPath.draw();
     };
 
-    updateAnchorsLayer = () => {
+    anchorOver = () => {
+        clearTimeout(this._hideTimeout);
         this._anchorLayer.draw();
+    };
+
+    anchorOut = () => {
+        clearTimeout(this._hideTimeout);
+        this._hideTimeout = setTimeout(() => {
+            this._anchors.start.visible(false);
+            this._anchors.control.visible(false);
+            this._anchors.end.visible(false);
+            this._anchorLayer.draw();
+        }, 1000);
     };
 
     addArrow() {
@@ -57,13 +83,13 @@ class Arrow {
         this._anchors.control.setDragMoveCb(this.drawArrow);
         this._anchors.end.setDragMoveCb(this.drawArrow);
 
-        this._anchors.start.setMouseOverCb(this.updateAnchorsLayer);
-        this._anchors.control.setMouseOverCb(this.updateAnchorsLayer);
-        this._anchors.end.setMouseOverCb(this.updateAnchorsLayer);
+        this._anchors.start.setMouseOverCb(this.anchorOver);
+        this._anchors.control.setMouseOverCb(this.anchorOver);
+        this._anchors.end.setMouseOverCb(this.anchorOver);
 
-        this._anchors.start.setMouseOutCb(this.updateAnchorsLayer);
-        this._anchors.control.setMouseOutCb(this.updateAnchorsLayer);
-        this._anchors.end.setMouseOutCb(this.updateAnchorsLayer);
+        this._anchors.start.setMouseOutCb(this.anchorOut);
+        this._anchors.control.setMouseOutCb(this.anchorOut);
+        this._anchors.end.setMouseOutCb(this.anchorOut);
 
         this._anchorLayer.add(this._anchors.start.getAnchor());
         this._anchorLayer.add(this._anchors.control.getAnchor());
