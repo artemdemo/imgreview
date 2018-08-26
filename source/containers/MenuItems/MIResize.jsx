50590b704fac4d830add503dafa31f09c2d75645
Artem Demo <artemdemo@gmail.com>
2018-08-26 22:01:35 +0300

    resize popup ui

--- source/containers/MenuItems/MIResize.jsx
+++ source/containers/MenuItems/MIResize.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Icon from '../../components/Icon/Icon';
+import Popup from '../../components/Popup/Popup';
 import MainMenuItem from '../../components/MainMenu/MainMenuItem';
 
 class MIResize extends React.PureComponent {
     constructor(props) {
         super(props);
+
+        this.state = {
+            width: '0',
+            height: '0',
+        };
+
+        this.popupRef = React.createRef();
     }
 
     onClick = () => {
-
+        this.popupRef.current.show();
     };
+
+    onPopupOpen = () => {
+        const { canvas } = this.props;
+        console.log(canvas);
+    };
+
+    onResize = () => {};
 
     render() {
         const { canvas } = this.props;
@@ -24,6 +39,46 @@ class MIResize extends React.PureComponent {
                         name='expand'
                     />
                 </MainMenuItem>
+                <Popup
+                    title='Resize image'
+                    ref={this.popupRef}
+                    buttons={[
+                        {
+                            className: 'btn btn-secondary',
+                            text: 'Cancel',
+                        },
+                        {
+                            className: 'btn btn-primary',
+                            text: 'Resize',
+                            onClick: this.onResize,
+                        },
+                    ]}
+                    onOpen={this.onPopupOpen}
+                    showCloseBtn={false}
+                >
+                    <div className='form-group'>
+                        <label htmlFor='img-width'>Width (px)</label>
+                        <input
+                            className='form-control'
+                            placeholder='Enter width'
+                            value={this.state.width}
+                            onChange={width => this.setState({ width })}
+                            ref={this.nameRef}
+                            id='img-width'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='img-height'>Height (px)</label>
+                        <input
+                            className='form-control'
+                            placeholder='Enter height'
+                            value={this.state.height}
+                            onChange={height => this.setState({ height })}
+                            ref={this.nameRef}
+                            id='img-height'
+                        />
+                    </div>
+                </Popup>
             </React.Fragment>
         );
     }
