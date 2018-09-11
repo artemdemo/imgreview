a603fb55476a18b15f5c1412b41f477497b646c2
Artem Demo <artemdemo@gmail.com>
2018-09-11 14:51:24 +0300

    attempt to calc next control position

--- source/canvas/Arrow/AnchorsGroup.js
+++ source/canvas/Arrow/AnchorsGroup.js
@@ -2,7 +2,44 @@ import Konva from 'konva';
 import Anchor from './Anchor';
 
 class AnchorsGroup {
-    static defineAnchors(stage, maxLength) {
+    /**
+     * Calculating "Inner product space"
+     * @link http://qaru.site/questions/465748/inner-angle-between-two-lines/2019402#2019402
+     * @link https://en.wikipedia.org/wiki/Inner_product_space
+     * @param prevPos {object}
+     * @param newPos {object}
+     * @param basePos {object}
+     * @return {number} in radians
+     */
+    static getAngle(prevPos, newPos, basePos) {
+        const deltaXA = prevPos.x - basePos.x;
+        const deltaXB = newPos.x - basePos.x;
+        const deltaYA = prevPos.y - basePos.y;
+        const deltaYB = newPos.y - basePos.y;
+        const lenA = Math.sqrt((deltaXA ** 2) + (deltaYA ** 2));
+        const lenB = Math.sqrt((deltaXB ** 2) + (deltaYB ** 2));
+        const denominator = lenA * lenB;
+        if (denominator === 0) {
+            return 0;
+        }
+        return Math.acos(
+            ((deltaXA * deltaXB) + (deltaYA * deltaYB)) / denominator
+        );
+    }
+
+    constructor() {
+        this._anchorLayer = null;
+        this._anchors = null;
+        this._prevPosition = {
+            start: { x: 0, y: 0 },
+            control: { x: 0, y: 0 },
+            end: { x: 0, y: 0 },
+        };
+
+        this._cbMap = new Map();
+    }
+
+    defineAnchors(stage, maxLength) {
         let startX = stage.attrs.width / 4;
         const startY = stage.attrs.height / 2;
 
@@ -15,18 +52,18 @@ class AnchorsGroup {
             endX = controlX + (maxLength / 2);
         }
 
+        this._prevPosition.start.x = startX;
+        this._prevPosition.start.y = startY;
+        this._prevPosition.control.x = controlX;
+        this._prevPosition.control.y = startY;
+        this._prevPosition.end.x = endX;
+        this._prevPosition.end.y = startY;
+
         return {
             start: new Anchor(startX, startY),
             control: new Anchor(controlX, startY),
             end: new Anchor(endX, startY),
         };
-    }
-
-    constructor() {
-        this._anchorLayer = null;
-        this._anchors = null;
-
-        this._cbMap = new Map();
     }
 
     /**
@@ -52,24 +89,57 @@ class AnchorsGroup {
      */
     addToStage(stage, maxLength) {
         this._anchorLayer = new Konva.Layer();
-        this._anchors = AnchorsGroup.defineAnchors(stage, maxLength);
+        this._anchors = this.defineAnchors(stage, maxLength);
 
         this._anchors.start.on(
             'dragmove',
             () => {
+                const startPos = this._anchors.start.getPosition();
+                const endPos = this._anchors.end.getPosition();
+                const angle = AnchorsGroup.getAngle(
+                    this._prevPosition.start,
+                    startPos,
+                    endPos,
+                );
+                const controlPos = this._anchors.control.getPosition();
+
+                // control position in new coordinate system
+                const currentControlPos = {
+                    x: controlPos.x - endPos.x,
+                    y: endPos.y - controlPos.y,
+                };
+                const nextControlPos = {
+                    x: (currentControlPos.x * Math.cos(angle)) - (currentControlPos.y * Math.sin(angle)),
+                    y: (currentControlPos.y * Math.cos(angle)) + (currentControlPos.x * Math.sin(angle)),
+                };
+                const newControlPos = {
+                    x: nextControlPos.x + endPos.x,
+                    y: endPos.y + nextControlPos.y,
+                };
+                console.log(controlPos);
+                console.log(newControlPos);
+                this._anchors.control.setPosition(
+                    newControlPos.x,
+                    newControlPos.y,
+                );
                 this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+                this._prevPosition.start = startPos;
             },
         );
         this._anchors.control.on(
             'dragmove',
             () => {
+                const position = this._anchors.control.getPosition();
                 this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+                this._prevPosition.control = position;
             },
         );
         this._anchors.end.on(
             'dragmove',
             () => {
+                const position = this._anchors.end.getPosition();
                 this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+                this._prevPosition.end = position;
             },
         );
 
