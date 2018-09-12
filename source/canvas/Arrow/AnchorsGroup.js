d04671126b60c8103e8224624977f024a2fdb585
Artem Demo <artemdemo@gmail.com>
2018-09-12 18:49:45 +0300

    reorgonized handlers

--- source/canvas/Arrow/AnchorsGroup.js
+++ source/canvas/Arrow/AnchorsGroup.js
@@ -46,6 +46,26 @@ class AnchorsGroup {
         this._cbMap = new Map();
     }
 
+    calculateControlPos(angleChange, centerPos) {
+        const controlPos = this._anchors.control.getPosition();
+
+        // control position in new coordinate system
+        const currentControlPos = {
+            x: controlPos.x - centerPos.x,
+            y: controlPos.y - centerPos.y,
+        };
+        const cosAngle = Math.cos(angleChange);
+        const sinAngle = Math.sin(angleChange);
+        const nextControlPos = {
+            x: (currentControlPos.x * cosAngle) - (currentControlPos.y * sinAngle),
+            y: (currentControlPos.y * cosAngle) + (currentControlPos.x * sinAngle),
+        };
+        return {
+            x: nextControlPos.x + centerPos.x,
+            y: centerPos.y + nextControlPos.y,
+        };
+    }
+
     defineAnchors(stage, maxLength) {
         let startX = stage.attrs.width / 4;
         const startY = stage.attrs.height / 2;
@@ -73,6 +93,52 @@ class AnchorsGroup {
         };
     }
 
+    moveStart = () => {
+        const startPos = this._anchors.start.getPosition();
+        const endPos = this._anchors.end.getPosition();
+        const startAngle = AnchorsGroup.getAngle(
+            startPos,
+            endPos,
+        );
+        const angleChange = startAngle - this._prevAngle.start;
+
+        const newControlPos = this.calculateControlPos(angleChange, endPos);
+
+        this._anchors.control.setPosition(
+            newControlPos.x,
+            newControlPos.y,
+        );
+        this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+        this._prevAngle.start = startAngle;
+        this._prevAngle.end = Math.PI + startAngle;
+    };
+
+    moveControl = () => {
+        const position = this._anchors.control.getPosition();
+        this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+        this._prevPosition.control = position;
+    };
+
+    moveEnd = () => {
+        const startPos = this._anchors.start.getPosition();
+        const endPos = this._anchors.end.getPosition();
+        const endAngle = AnchorsGroup.getAngle(
+            endPos,
+            startPos,
+        );
+        const angleChange = endAngle - this._prevAngle.end;
+
+        const newControlPos = this.calculateControlPos(angleChange, startPos);
+
+        this._anchors.control.setPosition(
+            newControlPos.x,
+            newControlPos.y,
+        );
+        this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
+        this._prevAngle.start = Math.PI + endAngle;
+        this._prevAngle.end = endAngle;
+    };
+
     /**
      * @public
      * @param isVisible {boolean}
@@ -91,26 +157,6 @@ class AnchorsGroup {
         this._anchorLayer.draw();
     }
 
-    calculateControlPos(angleChange, centerPos) {
-        const controlPos = this._anchors.control.getPosition();
-
-        // control position in new coordinate system
-        const currentControlPos = {
-            x: controlPos.x - centerPos.x,
-            y: controlPos.y - centerPos.y,
-        };
-        const cosAngle = Math.cos(angleChange);
-        const sinAngle = Math.sin(angleChange);
-        const nextControlPos = {
-            x: (currentControlPos.x * cosAngle) - (currentControlPos.y * sinAngle),
-            y: (currentControlPos.y * cosAngle) + (currentControlPos.x * sinAngle),
-        };
-        return {
-            x: nextControlPos.x + centerPos.x,
-            y: centerPos.y + nextControlPos.y,
-        };
-    }
-
     /**
      * @public
      */
@@ -118,58 +164,9 @@ class AnchorsGroup {
         this._anchorLayer = new Konva.Layer();
         this._anchors = this.defineAnchors(stage, maxLength);
 
-        this._anchors.start.on(
-            'dragmove',
-            () => {
-                const startPos = this._anchors.start.getPosition();
-                const endPos = this._anchors.end.getPosition();
-                const startAngle = AnchorsGroup.getAngle(
-                    startPos,
-                    endPos,
-                );
-                const angleChange = startAngle - this._prevAngle.start;
-
-                const newControlPos = this.calculateControlPos(angleChange, endPos);
-
-                this._anchors.control.setPosition(
-                    newControlPos.x,
-                    newControlPos.y,
-                );
-                this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
-                this._prevAngle.start = startAngle;
-                this._prevAngle.end = Math.PI + startAngle;
-            },
-        );
-        this._anchors.control.on(
-            'dragmove',
-            () => {
-                const position = this._anchors.control.getPosition();
-                this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
-                this._prevPosition.control = position;
-            },
-        );
-        this._anchors.end.on(
-            'dragmove',
-            () => {
-                const startPos = this._anchors.start.getPosition();
-                const endPos = this._anchors.end.getPosition();
-                const endAngle = AnchorsGroup.getAngle(
-                    endPos,
-                    startPos,
-                );
-                const angleChange = endAngle - this._prevAngle.end;
-
-                const newControlPos = this.calculateControlPos(angleChange, startPos);
-
-                this._anchors.control.setPosition(
-                    newControlPos.x,
-                    newControlPos.y,
-                );
-                this._cbMap.has('dragmove') && this._cbMap.get('dragmove')();
-                this._prevAngle.start = Math.PI + endAngle;
-                this._prevAngle.end = endAngle;
-            },
-        );
+        this._anchors.start.on('dragmove', this.moveStart);
+        this._anchors.control.on('dragmove', this.moveControl);
+        this._anchors.end.on('dragmove', this.moveEnd);
 
         this._anchorLayer.add(this._anchors.start.getAnchor());
         this._anchorLayer.add(this._anchors.control.getAnchor());
