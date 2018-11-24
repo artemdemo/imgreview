438bd1872063f75adfccad8eb1dd575fa8eac8ea
Artem Demo <artemdemo@gmail.com>
2018-11-24 20:27:14 +0200

    removed bootstrap, using only grid and reboot (normalizer)

--- source/index.jsx
+++ source/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-// `reset-css` here is part of preparation to remove `bootstrap`
-import 'reset-css';
 import store from './store';
 import AppView from './views/AppView';
 
