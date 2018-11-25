0fae2c94feb1e5a3b222b3208f29200cdee0c4b5
Artem Demo <artemdemo@gmail.com>
2018-11-25 21:51:54 +0200

    blur shapes on image click

--- source/canvas/CanvasImage.js
+++ source/canvas/CanvasImage.js
@@ -1,6 +1,14 @@
 import Konva from 'konva';
+import {
+    blurShapes,
+} from '../model/shapes/shapesActions';
+import store from '../store';
 
 class CanvasImage {
+    static clickHandler() {
+        store.dispatch(blurShapes());
+    }
+
     constructor(props) {
         this._image = new Konva.Image(props);
 
@@ -10,10 +18,12 @@ class CanvasImage {
     addToStage(stage) {
         this._layer = new Konva.Layer();
         this._layer.add(this._image);
+        this._layer.on('click', CanvasImage.clickHandler);
         stage.add(this._layer);
     }
 
     destroy() {
+        this._layer.off('click', CanvasImage.clickHandler);
         this._image.destroy();
         this._layer.destroy();
     }
