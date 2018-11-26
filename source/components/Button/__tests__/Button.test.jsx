eaeba1e938509b38130df33ca1b1f3a48924a2b8
Artem Demo <artemdemo@gmail.com>
2018-11-26 08:00:07 +0200

    added test for Button

---
+++ source/components/Button/__tests__/Button.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import Button from '../Button';
+
+describe('Button', () => {
+    it('empty render', () => {
+        const tree = renderer.create(
+            <Button />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render with text', () => {
+        const tree = renderer.create(
+            <Button>Some text</Button>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render with title', () => {
+        const tree = renderer.create(
+            <Button title='some title'>Some text</Button>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render with primary property', () => {
+        const tree = renderer.create(
+            <Button primary>Some text</Button>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render with secondary property', () => {
+        const tree = renderer.create(
+            <Button secondary>Some text</Button>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should handle onClick', () => {
+        const onClickMock = jest.fn();
+        const wrapper = mount(
+            <Button onClick={onClickMock}>Some text</Button>,
+        );
+        wrapper.find('button').simulate('click');
+        expect(onClickMock).toBeCalled();
+    });
+});
