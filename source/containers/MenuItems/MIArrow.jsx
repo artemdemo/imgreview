3246e93e4725146cb9fc4114d4224ad84f3b64bf
Artem Demo <artemdemo@gmail.com>
2019-04-03 08:05:26 +0300

    reorganized Arrow connection

--- source/containers/MenuItems/MIArrow.jsx
+++ source/containers/MenuItems/MIArrow.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Icon from '../../components/Icon/Icon';
 import MainMenuItem from '../../components/MainMenu/MainMenuItem';
-import Arrow from '../../canvas/Arrow/Arrow.ts';
 import { connectArrow } from '../../model/connectShape';
 
 class MIArrow extends React.PureComponent {
     onClick = () => {
-        const { shapes } = this.props;
-        const arrow = new Arrow({
-            stroke: shapes.stroke,
-            strokeWidth: shapes.strokeWidth,
-        });
-        connectArrow(arrow);
+        connectArrow();
     };
 
     render() {
@@ -34,6 +28,5 @@ class MIArrow extends React.PureComponent {
 export default connect(
     state => ({
         canvas: state.canvas,
-        shapes: state.shapes,
     }),
 )(MIArrow);
--- source/model/connectShape.js
+++ source/model/connectShape.js
@@ -4,9 +4,14 @@ import store from '../store';
 import { cursorTypes } from './canvas/canvasConst';
 import { blurShapes, addArrow } from './shapes/shapesActions';
 import { setCursor } from './canvas/canvasActions';
+import Arrow from '../canvas/Arrow/Arrow.ts';
 
-export const connectArrow = (arrow) => {
-    const { canvas } = store.getState();
+export const connectArrow = () => {
+    const { canvas, shapes } = store.getState();
+    const arrow = new Arrow({
+        stroke: shapes.stroke,
+        strokeWidth: shapes.strokeWidth,
+    });
     arrow.addToStage(canvas.stage);
     arrow.on('click', arrowInstance => store.dispatch(blurShapes(arrowInstance)));
     arrow.on('dragstart', arrowInstance => store.dispatch(blurShapes(arrowInstance)));
