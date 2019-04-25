0f8e9ca35fe7dcf2a86d6e8eb984c50817988bdd
Artem Demo <artemdemo@gmail.com>
2019-04-25 20:38:27 +0300

    converted FormButtonsRow to styled component

--- source/components/FormButtonsRow/FormButtonsRow.jsx
+++ source/components/FormButtonsRow/FormButtonsRow.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './FormButtonsRow.less';
+const FormButtonSty = styled.div`
+    margin-right: 14px;
+    display: inline-block;
+
+    &:last-child {
+        margin-right: 0;
+    }
+`;
 
 const FormButtonsRow = props => (
     <React.Fragment>
         {React.Children.map(props.children, child => (
-            <div className='form-buttons-row-child'>
+            <FormButtonSty>
                 {child}
-            </div>
+            </FormButtonSty>
         ))}
     </React.Fragment>
 );
