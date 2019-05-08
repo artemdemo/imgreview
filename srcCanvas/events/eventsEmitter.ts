5f0c32ca60b7a78ae2648abfee6d3c1a392595ce
Artem Demo <artemdemo@gmail.com>
2019-05-08 21:22:05 +0300

    added error handling in eventsEmitter

--- srcCanvas/events/eventsEmitter.ts
+++ srcCanvas/events/eventsEmitter.ts
@@ -2,4 +2,18 @@ import NanoEvents from 'nanoevents'
 
 const emitter = new NanoEvents();
 
-export default emitter;
+export default {
+    emit(...args) {
+        if (args[0] === undefined) {
+            throw new Error('Can\'t "emit" undefined key');
+        }
+        emitter.emit(...args);
+    },
+
+    on(...args) {
+        if (args[0] === undefined) {
+            throw new Error('Can\'t subscribe to undefined key');
+        }
+        emitter.on(...args);
+    },
+};
