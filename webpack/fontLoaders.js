eeee8b90935e331e5fc7f688af23073196f1489a
Artem Demo <artemdemo@gmail.com>
2019-06-07 21:54:50 +0300

    updated url-loader

---
+++ webpack/fontLoaders.js
@@ -0,0 +1,35 @@
+const getLoaders = mimetype => ([
+    {
+        loader: 'url-loader',
+        options: {
+            limit: 65000,
+            mimetype,
+            name: 'fonts/[name].[ext]',
+        },
+    },
+]);
+
+const fontLoaders = [
+    {
+        test: /\.(svg)(\?.*$|$)/,
+        use: getLoaders('image/svg+xml'),
+    },
+    {
+        test: /\.(woff)(\?.*$|$)/,
+        use: getLoaders('application/font-woff'),
+    },
+    {
+        test: /\.(woff2)(\?.*$|$)/,
+        use: getLoaders('application/font-woff2'),
+    },
+    {
+        test: /\.([ot]tf)(\?.*$|$)/,
+        use: getLoaders('application/octet-stream'),
+    },
+    {
+        test: /\.(eot)(\?.*$|$)/,
+        use: getLoaders('application/vnd.ms-fontobject'),
+    },
+];
+
+module.exports = fontLoaders;
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,29 +9,7 @@ const {
     ModuleConcatenationPlugin,
 } = require('webpack').optimize;
 const extractStyles = require('./extractStyles');
-
-const fontLoaders = [
-    {
-        test: /\.(svg)(\?.*$|$)/,
-        use: 'url-loader?limit=65000&mimetype=image/svg+xml&name=fonts/[name].[ext]',
-    },
-    {
-        test: /\.(woff)(\?.*$|$)/,
-        use: 'url-loader?limit=65000&mimetype=application/font-woff&name=fonts/[name].[ext]',
-    },
-    {
-        test: /\.(woff2)(\?.*$|$)/,
-        use: 'url-loader?limit=65000&mimetype=application/font-woff2&name=fonts/[name].[ext]',
-    },
-    {
-        test: /\.([ot]tf)(\?.*$|$)/,
-        use: 'url-loader?limit=65000&mimetype=application/octet-stream&name=fonts/[name].[ext]',
-    },
-    {
-        test: /\.(eot)(\?.*$|$)/,
-        use: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]',
-    },
-];
+const fontLoaders = require('./fontLoaders');
 
 /**
  * @param options {Object}
