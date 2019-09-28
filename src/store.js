3942d3ff95b48151b7e3b37b811307bc8fb7eee8
Artem Demo <artemdemo@gmail.com>
2019-09-28 12:32:49 +0300

    introduced "isDev" from "env.js"

--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
+import { isDev } from './services/env';
 
 import combinedReducers from './reducers.ts';
 
 const middlewares = [];
 
-try {
-    if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.log('logger is disabled');
-        // const { logger } = require('redux-logger');
-        // middlewares.push(logger);
-    }
-} catch (e) {}
+if (isDev) {
+    // eslint-disable-next-line no-console
+    console.log('logger is disabled');
+    // const { logger } = require('redux-logger');
+    // middlewares.push(logger);
+}
 
 const store = createStore(
     combinedReducers,
