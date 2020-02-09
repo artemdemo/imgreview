27ba0f89a1d8b7332bcfcc5e5ccc3e49124e1f86
Artem Demo <artemdemo@gmail.com>
2020-02-09 20:41:33 +0200

    removed unused mocks

--- src/model/canvas/__mocks__/canvasActions.js
+++
@@ -1,5 +0,0 @@
-export const setStage = jest.fn();
-export const addImage = jest.fn();
-export const updateImageSize = jest.fn();
-export const saveCanvas = jest.fn();
-export const setCursor = jest.fn();
