8fdc55afde87b486ad5128f5425dd9ca812f26d0
Artem Demo <artemdemo@gmail.com>
2020-05-24 07:09:43 +0300

    updated mock for font-awesome

--- src/__mocks__/@fortawesome/react-fontawesome.js
+++ src/__mocks__/@fortawesome/react-fontawesome.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const FontAwesomeIcon = props => (
     <span data-mock='FontAwesomeIcon' >
-        {props.icon}
+        {JSON.stringify(props, null, 2)}
     </span>
 );
