d1a58bbaef65bbd5f8f2be98a63ba3193e1ad210
Artem Demo <artemdemo@gmail.com>
2020-06-24 08:32:04 +0300

    updated creat shape flow

--- src/containers/MenuItems/__tests__/MIArrow.test.jsx
+++ src/containers/MenuItems/__tests__/MIArrow.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { mount } from "enzyme";
 import renderer from "react-test-renderer";
 import MIArrow from "../MIArrow.tsx";
+import * as canvasApi from '../../../../srcCanvas/api';
 
 jest.mock('react-redux');
 jest.mock('../../../components/TopMenu/TopMenuItem');
@@ -35,16 +36,24 @@ describe('MIArrow', () => {
     });
 
     it('should handle click', () => {
+        const strokeColor = 'red';
+        const strokeWidth = 5;
         const wrapper = mount(
             <MIArrow
                 menu={{
-                    strokeColor: 'red',
-                    strokeWidth: 5,
+                    strokeColor,
+                    strokeWidth,
                 }}
             />
         );
         wrapper.simulate('click');
-        expect(canvasApi.createShape).toBeCalled();
+        expect(canvasApi.startAddingShape).toBeCalledWith(
+            canvasApi.EShapeTypes.ARROW,
+            {
+                strokeColor,
+                strokeWidth,
+            },
+        );
         expect(ganalyticsMock.sendEvent).toBeCalledWith({
             eventAction: 'add Arrow',
             eventCategory: 'MenuClick',
