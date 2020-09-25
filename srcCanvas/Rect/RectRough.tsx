530683362c74265949977d1b62a756063991bf3c
Artem Demo <artemdemo@gmail.com>
2020-09-25 21:13:42 +0300

    added cloning functionality

--- srcCanvas/Rect/RectRough.tsx
+++ srcCanvas/Rect/RectRough.tsx
@@ -168,9 +168,20 @@ class RectRough extends Rect {
         });
     }
 
-    // clone(): RectRough {
-    //
-    // }
+    clone(): RectRough {
+        const attrs = this.shape?.getAttrs();
+        return new RectRough({
+            ...this.props,
+            ...(attrs && {
+                x: attrs.x,
+                y: attrs.y,
+                width: attrs.width,
+                height: attrs.height,
+                stroke: attrs.stroke,
+                strokeWidth: attrs.strokeWidth,
+            }),
+        });
+    }
 }
 
 export default RectRough;
