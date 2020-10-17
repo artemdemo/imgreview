00fda84fcaf4be550d6fdb377b4c7310aa49e7a3
Artem Demo <artemdemo@gmail.com>
2020-10-17 14:14:48 +0300

    remove static files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -112,7 +112,9 @@ module.exports = (options) => {
                 dry: false,
                 cleanOnceBeforeBuildPatterns: [
                     './index.html',
+                    './about/index.html',
                     './200.html',
+                    './404.html',
                     `${options.buildFolder}/**/*`,
                     `!${options.buildFolder}/.gitignore`,
                 ],
