8d0ef67d8925c00a73381646971c76ebc0b6eed9
Artem Demo <artemdemo@gmail.com>
2021-12-09 14:42:05 +0200

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = () => {
         : './js/[id].chunk.js',
       publicPath: '/',
     },
+    devtool: 'eval-source-map',
     devServer: {
       host: '0.0.0.0',
       port: 4001,
