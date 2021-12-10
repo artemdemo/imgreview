086b22199b98a6866830d91f8f26c8f299f0436f
Artem Demo <artemdemo@gmail.com>
2021-12-10 14:53:01 +0200

    removed redux-logger

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
-import { isDev } from './services/env';
 
 import combinedReducers from './reducers';
 
 const middlewares: any[] = [];
-
-if (isDev) {
-  // eslint-disable-next-line no-console
-  console.log('logger is disabled');
-  // const { logger } = require('redux-logger');
-  // middlewares.push(logger);
-}
 
 const store = createStore(combinedReducers, applyMiddleware(...middlewares));
 
