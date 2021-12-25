27dd2b4185ee9da2c7a7beff8f61e3ecd2c32ba5
Artem Demo <artemdemo@gmail.com>
2021-12-25 18:13:13 +0200

    fixed bug with draggable canvas and shapes

--- srcCanvas/addShape.ts
+++ srcCanvas/addShape.ts
@@ -36,6 +36,12 @@ const attachGeneralEvents = (shape: Shape) => {
   });
   shape.on('mouseover', () => {
     const { shapes, stage } = <TCanvasState>canvasStore.getState();
+    if (stage.isDraggable) {
+      shape.draggable(false);
+    } else {
+      // While adding shape user shouldn't be able to interact with existing shapes.
+      shape.draggable(!shapes.addingShapeRef);
+    }
     const cursor = shapes.addingShapeRef
       ? ECursorTypes.AUTO
       : ECursorTypes.MOVE;
