2a1bef5a15bfbb275d520a7fefe22621fd083703
Artem Demo <artemdemo@gmail.com>
2021-12-27 19:21:55 +0200

    fixed typo

--- src/containers/CanvasContainer/HowToStart.tsx
+++ src/containers/CanvasContainer/HowToStart.tsx
@@ -4,7 +4,7 @@ import './HowToStart.less';
 export const HowToStart: React.FC = () => {
   return (
     <div className="HowToStart">
-      Start by dropping image here (or draw shape)
+      Start by dropping image here (or draw a shape)
     </div>
   );
 };
