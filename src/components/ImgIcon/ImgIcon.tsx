55d36927afbe26db010408546f09cc27a3be7043
Artem Demo <artemdemo@gmail.com>
2022-01-07 19:03:29 +0200

    updated icon pos calculation for narrow icons

--- src/components/ImgIcon/ImgIcon.tsx
+++ src/components/ImgIcon/ImgIcon.tsx
@@ -34,17 +34,34 @@ export const ImgIcon: React.FC<Props> = (props) => {
   const { icon } = props;
   const wrapperEl = useRef<HTMLSpanElement>(null);
 
+  const getCurrentIconWidth = () => {
+    if (icon === EIcon.chevronDown) {
+      return 10;
+    }
+    return ICON_SIZE;
+  };
+
+  const getCurrentIconLeft = () => {
+    if (icon === EIcon.chevronDown) {
+      return -1 * Number(icon) * ICON_SIZE - 7;
+    }
+    return -1 * Number(icon) * ICON_SIZE;
+  };
+
   useEffect(() => {
     // Enum in TS has keys for key and value.
     // Meaning there are exactly 2 times more keys than actually are.
     const iconsAmount = Object.keys(EIcon).length / 2;
 
-    wrapperEl.current?.style.setProperty('--img-icon-size', `${ICON_SIZE}px`);
+    wrapperEl.current?.style.setProperty('--img-icon-width', `${getCurrentIconWidth()}px`);
+    wrapperEl.current?.style.setProperty('--img-icon-height', `${ICON_SIZE}px`);
     wrapperEl.current?.style.setProperty(
       '--img-icon-collection-width',
       `${ICON_SIZE * iconsAmount}px`,
     );
   }, []);
+
+  const iconIdx = Number(icon);
 
   return (
     <span className="ImgIconWrapper" ref={wrapperEl}>
@@ -52,7 +69,7 @@ export const ImgIcon: React.FC<Props> = (props) => {
         className="ImgIcon"
         style={{
           backgroundImage: `url('${img}')`,
-          left: -1 * Number(icon) * ICON_SIZE,
+          left: getCurrentIconLeft(),
         }}
       />
     </span>
