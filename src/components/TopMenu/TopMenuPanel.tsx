c428d94f62360dbfbc9d5d20cf40c082b0c99cc4
Artem Demo <artemdemo@gmail.com>
2022-01-10 09:17:17 +0200

    updated dom positions

--- src/components/TopMenu/TopMenuPanel.tsx
+++ src/components/TopMenu/TopMenuPanel.tsx
@@ -7,6 +7,9 @@ const TopMenuPanel = styled(ClearFix)`
   padding: 4px 15px;
   background-color: ${styleVars.mainMenuColor};
   z-index: ${styleVars.mainMenuZIndex};
+  position: fixed;
+  left: 0;
+  right: 0;
 `;
 
 export default TopMenuPanel;
