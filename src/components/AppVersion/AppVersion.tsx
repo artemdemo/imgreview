8fc8ec1b954781f3f53d916eb6fa7c3170112ba2
Artem Demo <artemdemo@gmail.com>
2022-01-26 08:35:57 +0200

    simplified AppVersion

--- src/components/AppVersion/AppVersion.tsx
+++ src/components/AppVersion/AppVersion.tsx
@@ -5,11 +5,9 @@ export const AppVersion: React.FC = () => {
   const [version, setVersion] = useState<string>('x.xx');
 
   useEffect(() => {
-    try {
-      if (process.env.appVersion) {
-        setVersion(process.env.appVersion);
-      }
-    } catch (e) {}
+    if (process.env.appVersion) {
+      setVersion(process.env.appVersion);
+    }
   }, []);
 
   return <div className={s.AppVersion}>{version}</div>;
