6f0acf2a25b2fe8f17b786761267a3384b5b5331
Artem Demo <artemdemo@gmail.com>
2022-01-27 19:04:36 +0200

    moved AppStateProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Header } from '../src/containers/Header/Header';
+import { AppStateProvider } from '../src/model/AppStateContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AppStateProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </AppStateProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { App } from '../src/App';
-import { AppStateProvider } from '../src/model/AppStateContext';
 
 const Index: NextPage = () => {
   return (
-    <>
-      <AppStateProvider>
-        <App />
-      </AppStateProvider>
-    </>
+    <App />
   );
 };
 
