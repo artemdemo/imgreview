96c1c0ffca5e54d177bf15ef408dc86959020e55
Artem Demo <artemdemo@gmail.com>
2022-01-27 19:02:16 +0200

    removed duplicate Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { App } from '../src/App';
-import { Header } from '../src/containers/Header/Header';
 import { AppStateProvider } from '../src/model/AppStateContext';
 
 const Index: NextPage = () => {
   return (
     <>
-      <Header />
       <AppStateProvider>
         <App />
       </AppStateProvider>
