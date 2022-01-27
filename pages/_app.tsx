aa8897dff5e0fcb14d66137cfd38a8f2b029349c
Artem Demo <artemdemo@gmail.com>
2022-01-27 18:56:05 +0200

    moved Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 // I'm not using all bootstrap, only grid and normalizer
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
-
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Header } from '../src/containers/Header/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
