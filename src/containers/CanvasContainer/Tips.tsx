5d0503530b758f2f3efbe4db6cd7e0f93cdfccf4
Artem Demo <artemdemo@gmail.com>
2022-01-27 19:00:50 +0200

    refactored global css variables

--- src/containers/CanvasContainer/Tips.tsx
+++ src/containers/CanvasContainer/Tips.tsx
@@ -4,9 +4,12 @@ import s from './Tips.module.css';
 export const Tips: React.FC = () => {
   return (
     <div className={s.Tips}>
-      Psst...<br />
-      You can add as many images as you want here.<br />
-      If you want to read more about the features click here.<br />
+      Psst...
+      <br />
+      You can add as many images as you want here.
+      <br />
+      If you want to read more about the features click here.
+      <br />
     </div>
-  )
+  );
 };
