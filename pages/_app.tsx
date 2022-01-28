838cfda17145cfe8464debac38b6b933d2fd32f0
Artem Demo <artemdemo@gmail.com>
2022-01-28 11:28:30 +0200

    removed bootstrap grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 // I'm not using all bootstrap, only grid and normalizer
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import '../styles/globals.css';
 import React from 'react';
