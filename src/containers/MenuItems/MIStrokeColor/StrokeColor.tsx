f93b82c962bb68b19024c58d62325ab8b78757cd
Artem Demo <artemdemo@gmail.com>
2022-02-03 19:53:34 +0200

    handle low alpha for the stroke color

--- src/containers/MenuItems/MIStrokeColor/StrokeColor.tsx
+++ src/containers/MenuItems/MIStrokeColor/StrokeColor.tsx
@@ -1,20 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Color from 'color';
+import classnames from 'classnames';
 import { AppStateContext } from '../../../model/AppStateContext';
 import s from './StrokeColor.module.css';
 
 export const StrokeColor: React.FC = () => {
   const {
-    state: { menu },
+    state: {
+      menu: { strokeColor },
+    },
   } = useContext(AppStateContext);
+  const [isTransparent, setIsTransparent] = useState(false);
+
+  useEffect(() => {
+    setIsTransparent(Color(strokeColor).alpha() < 0.15);
+  }, [strokeColor]);
 
   return (
     <div
-      className={s.StrokeColor}
+      className={classnames({
+        [s.StrokeColor]: true,
+        [s.StrokeColor_isTransparent]: isTransparent,
+      })}
       style={{
-        backgroundColor: menu.strokeColor,
+        backgroundColor: strokeColor,
       }}
     >
-      <div className={s.StrokeColor__Plug} />
+      <div
+        className={classnames({
+          [s.StrokeColor__Plug]: true,
+          [s.StrokeColor__Plug_isTransparent]: isTransparent,
+        })}
+      />
     </div>
   );
 };
