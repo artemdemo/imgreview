bf9ccf196226b520310c033f60b43a80be939ee8
Artem Demo <artemdemo@gmail.com>
2022-02-26 11:45:03 +0200

    refactored _document

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="app-version" content={process.env.appVersion} />
+        <meta
+          name="description"
+          content="App to mark and annotate your images and screenshots."
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import { useDevQueries } from '../../services/url';
 
 declare global {
@@ -30,27 +31,8 @@ export const Header: React.FC = () => {
   return (
     <Head>
       <title>ImgReview</title>
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="app-version" content={process.env.appVersion} />
-      <meta
-        name="description"
-        content="App to mark and annotate your images and screenshots."
-      />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="icon" type="image/png" href="/favicon.png" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lato:wght@400&display=swap"
-        rel="stylesheet"
-      />
-
       {attachGaScript && (
-        <script
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-38910005-11"
         />
